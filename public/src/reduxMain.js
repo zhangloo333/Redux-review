6ae8881292166a1d679c973eaa59f6e1742fd5ec
zhangloo333@gmail.com <zhangloo333@gmail.com>
2017-06-14 16:41:44 -0700

    怎么删除 array里面的元素，这里面我们用的是 filter(function(e){return true or flase}}) 如果是true 保留，如果不是的话就丢掉

--- public/src/reduxMain.js
+++ public/src/reduxMain.js
@@ -42,6 +42,21 @@ let reduer = (state,action) => {
           }
         ]
       }
+      //使用filter，如果filter中的方程返回的是true,的情况下，就保留，否则删除
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter(function(hobby){
+          return hobby.id !== action.id;
+        })
+      };
+
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
+
     default:
       return state;
   }
@@ -76,6 +91,11 @@ store.dispatch({
 })
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'setting'
+})
+
+store.dispatch({
   type: 'CHANG_NAME',
   name: 'yi'
 })
@@ -84,4 +104,20 @@ store.dispatch({
   type: 'ADD_MOVIE',
   title: 'Mad Max',
   genre: 'Action'
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'litter red',
+  genre: 'drama'
+})
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id:2
+})
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id:1
 })
